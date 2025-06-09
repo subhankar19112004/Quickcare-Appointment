cdc1797ff690301a9c415cb12f7e6de2bab637e3
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2025-06-09 17:48:03 +0530

    added login for admin functionality in frontend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/adminContext';
 
 function App() {
-  return (
-    <div>App</div>
+
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000} // Reduce the time the toast stays visible to 3 seconds
+        hideProgressBar={false} // Show the progress bar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        toastClassName="custom-toast" // Custom toast style
+        bodyClassName="custom-toast-body" // Custom body style
+      />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000} // Reduce the time the toast stays visible to 3 seconds
+        hideProgressBar={false} // Show the progress bar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        toastClassName="custom-toast" // Custom toast style
+        bodyClassName="custom-toast-body" // Custom body style
+      />
+    </>
   )
 }
 
