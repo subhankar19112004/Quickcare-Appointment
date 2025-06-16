393f901417d4d93ce32e1f06021ee85849d94800
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2025-06-16 20:14:24 +0530

    added route handler

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { getProfile, loginUser, registerUser } from '../controllers/userController.js';
+import { getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
+import upload from '../middlewares/multer.js';
 
 
 const userRouter = express.Router();
@@ -8,6 +9,7 @@ const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/get-profile',authUser, getProfile);
+userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile);
 
 
 export default userRouter;
