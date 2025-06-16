6261057e72ae3554076a903ec9bd5d89ced25e96
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2025-06-16 20:11:31 +0530

    updated navbar with backend fetching

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { assets } from '../assets/assets_frontend/assets.js'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext.jsx';
 
+
 const Navbar = () => {
 
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
-    const { token, setToken } = useContext(AppContext);
+    const { token, setToken, userData } = useContext(AppContext);
 
     const logout = () => {
         setToken(false);
@@ -41,9 +42,9 @@ const Navbar = () => {
 
             <div className='flex items-center  transition-all duration-300 gap-4'>
                 {
-                    token ?
+                    token && userData ?
                         <div className='flex items-center gap-2 cursor-pointer group relative'>
-                            <img className='w-8 rounded-full' src={assets.profile_pic} />
+                            <img className='w-8 rounded-full ' src={userData?.image} />
                             <img className='w-2.5' src={assets.dropdown_icon} />
                             <div className='absolute  top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                                 <div className='min-w-45 bg-stone-100 rounded flex flex-col gap-4 p-4'>
